c048cba5a9c66532282436cd5513890a4313fc1f
Mohed Alrahbi <mccdama@gmail.com>
2023-04-04 00:59:58 +0200

    Go Server - Missed Files

--- go/main.go
+++ go/main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
-	http.HandleFunc("/ping", handler)
-	log.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type Pass struct {
+	Orginal string `json:"orginal" xml:"orginal"`
+	Hash    string `json:"hash" xml:"hash"`
 }
 
-func handler(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+func main() {
 
-	hash, _ := hashPassword("test")
-	w.Write([]byte("A hashed password for 'test' " + hash))
+	e := echo.New()
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Skipper: func(c echo.Context) bool {
+	// 		if strings.HasPrefix(c.Request().Host, "localhost") {
+	// 			return true
+	// 		}
+	// 		return false
+	// 	},
+	// }))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong!")
+	})
+	e.POST("/hash", generateHashedPass)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func generateHashedPass(c echo.Context) error {
+	rawPassword := c.FormValue("rawPassword")
+	hash, _ := hashPassword(rawPassword)
+
+	u := &Pass{
+		Orginal: rawPassword,
+		Hash:    hash,
+	}
+
+	return c.JSON(http.StatusOK, u)
+
+	//return c.HTML(http.StatusOK, "<b>Thank you! "+hash+"</b>")
 }
 
 func hashPassword(password string) (string, error) {
