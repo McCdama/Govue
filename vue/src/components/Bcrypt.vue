2e602cf6bc5bbc906c12afeb6ac87739402967c7
Mohed Alrahbi <mccdama@gmail.com>
2023-04-06 03:18:35 +0200

    Code Refactoring

--- vue/src/components/Bcrypt.vue
+++ vue/src/components/Bcrypt.vue
@@ -5,28 +5,28 @@
         <h2 class="inline sm:block">Want secure your password?</h2>
       </div>
       <form class="mt-10 max-w-md">
+
         <div class="flex gap-x-4">
           <label for="password" class="sr-only">Your Password</label>
           <input v-model="password" id="password" name="password" type="text" autocomplete="off" class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Enter your Password" />
+         
           <label for="rounds" class="sr-only">Rounds</label>
           <input v-model="rounds" id="rounds" name="rounds" type="number" autocomplete="off" class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Round"  min="10" max="31" />
+        
           <button @click="generateBcrypt" type="button" class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Hash it!</button>
         </div>
-
-
+        
         <div>
-    <label for="bcryptedPass" class="mt-5 block text-sm font-medium leading-6 text-gray-900" v-if="bcryptedPass">Bcrypted Password</label>
-    <div class="mt-2">
-      <textarea  v-if="bcryptedPass" v-model="bcryptedPass" rows="4" name="bcryptedPass" id="bcryptedPass" class="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6" />
-    </div>
-  </div>
+            <label for="bcryptedPass" class="mt-5 block text-sm font-medium leading-6 text-gray-900" v-if="bcryptedPass">Bcrypted Password</label>
+            <div class="mt-2">
+                <textarea  v-if="bcryptedPass" v-model="bcryptedPass" rows="4" name="bcryptedPass" id="bcryptedPass" class="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6" />
+            </div>
+        </div>
 
       </form>
     </div>
   </div>
 </template>
-
-
 
 <script setup>
 import axios from 'axios'
---
+++ vue/src/components/HashPass.vue
@@ -0,0 +1,60 @@
+<template>
+    <form >
+      <div class="space-y-12 sm:space-y-16">
+        <div>
+          <h2 class="text-base font-semibold leading-7 text-gray-900">Gibberish Tool</h2>
+          <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-600">A hash tool to ensure immutability</p>
+          <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">Your
+              Password</label>
+            <div class="mt-2 sm:col-span-2 sm:mt-0">
+              <input type="text" autocomplete="off" v-model="rawPassword"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" />
+            </div>
+  
+            <!-- invisible -->
+            <label v-if="hashedPassword && rawPassword" for="hashed-password" class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"> 
+              Hashed Password</label>
+            <div class="mt-2 sm:col-span-2 sm:mt-0" v-if="hashedPassword && rawPassword">
+              <input type="text" autocomplete="off" v-model="hashedPassword"
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" @click="generateHash" class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <CheckCircleIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+          GENERATE
+        </button>
+      </div>
+    </form>
+  </template>
+  
+  <script setup>
+  import { CheckCircleIcon } from '@heroicons/vue/20/solid'
+  import axios from 'axios'
+  
+  import { ref } from 'vue'
+  
+  const rawPassword = ref('')
+  const hashedPassword = ref('')
+  
+  function generateHash() {
+  
+    axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8080/hash',
+    data: {
+      rawPassword: rawPassword.value,
+    }, 
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+  }) .then(function (res) {
+      hashedPassword.value = res.data.hash
+    });
+  }
+  
+  </script>
