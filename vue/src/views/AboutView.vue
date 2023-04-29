ccab73a0ef93e8561129f4a54abaad57a038f0ff
Mohed <mccdama@gmail.com>
2023-04-29 17:18:08 +0200

    Component Resolvers

---
+++ vue/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Checkmark16Filled from '@vicons/fluent/Checkmark16Filled'
+
+</script>
+
+<template>
+    <n-icon :component="Checkmark16Filled" size="40" :depth="1" />
+</template>
